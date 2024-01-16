72a2cd8c4d8828199d837e231986b248b683ab40
Preeti Raj <satendra11kum@gmail.com>
2024-01-16 15:17:07 +0530

    Connect BC to FE

---
+++ ConnectBackendWithFrontend/Server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const port = 4000;
+app.get("/",(req,res)=>{
+    res.send("Home Page")
+})
+app.listen(port,()=>{
+ console.log(`App are listening port at localhost:${port} `)
+})
